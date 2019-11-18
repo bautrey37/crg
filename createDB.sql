e13f783bb9553793651d948d8ac6a6191bcade02
Brandon Autrey <brandon.c.autrey@gmail.com>
2019-11-18 19:10:12 +0200

    Update createDB.sql

--- createDB.sql
+++ createDB.sql
@@ -2,12 +2,32 @@ DROP DATABASE IF EXISTS CAR_RENTAL_GATEWAY;
 CREATE DATABASE CAR_RENTAL_GATEWAY;
 USE CAR_RENTAL_GATEWAY;
 
+CREATE TABLE DISTRIBUTOR (
+	distributor_id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    distributor_name VARCHAR(30),
+	distributor_type VARCHAR(30)
+);
+
+INSERT INTO DISTRIBUTOR (distributor_name, distributor_type)
+VALUES ("Ryanair","airline");
+INSERT INTO DISTRIBUTOR (distributor_name, distributor_type)
+VALUES ("Expedia","car");
+
 CREATE TABLE BOOKING (
     booking_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     distributor_id INT(6),
     booking_name VARCHAR(30),
     location VARCHAR(100)
 );
+INSERT INTO BOOKING (distributor_id, booking_name, location)
+VALUES (1, "car rental A", "Tartu");
+INSERT INTO BOOKING (distributor_id, booking_name, location)
+VALUES (2, "car rental B", "Tallinn");
+
+CREATE VIEW RyanairEmployees AS
+SELECT * FROM BOOKING WHERE distributor_id = 1;
+CREATE VIEW ExpediaEmployees AS
+SELECT * FROM BOOKING WHERE distributor_id = 2;
 
 DROP DATABASE IF EXISTS USA_CUSTOMER;
 CREATE DATABASE USA_CUSTOMER;
@@ -20,4 +40,22 @@ CREATE TABLE USER (
     email_address VARCHAR(50),
     date_of_birth TIMESTAMP,
     telephone_number VARCHAR(10)
-)
+);
+
+INSERT INTO USER (full_name, home_address, email_address, date_of_birth, telephone_number)
+VALUES (
+    "Brandon Autrey", 
+    "Raatuse 22, Tartu, Estonia", 
+    "brandon.autrey@gmail.com", 
+    CURRENT_TIME(), 
+    "55778812"
+);
+
+INSERT INTO USER (full_name, home_address, email_address, date_of_birth, telephone_number)
+VALUES (
+    "John Doe", 
+    "Raatuse 22 Tartu, Estonia", 
+    "john.doe@gmail.com", 
+    CURRENT_TIME(), 
+    "72947482"
+);
